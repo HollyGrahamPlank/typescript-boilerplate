a04e5546b1b319c88a8519c31be3d93e1538d376
HollyGrahamPlank <hollygrahamplank@gmail.com>
2024-04-18 12:32:53 -0500

    Got testing working

---
+++ packages/shared-things/__tests__/index.test.ts
@@ -0,0 +1,10 @@
+import someText from "../src";
+
+describe("the shared text", () => {
+  it("should be hello world", () => {
+    expect(someText).toEqual("Hello World!");
+  });
+  it("is truthy", () => {
+    expect(someText).toBeTruthy();
+  });
+});
---
+++ packages/shared-things/jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "@jest/types";
+import { pathsToModuleNameMapper } from "ts-jest";
+// Load the config which holds the path aliases.
+// eslint-disable-next-line import/no-relative-packages
+import { compilerOptions } from "../../tsconfig.json";
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    // This has to match the baseUrl defined in tsconfig.json.
+    prefix: "<rootDir>/../../",
+  }),
+};
+
+export default config;
